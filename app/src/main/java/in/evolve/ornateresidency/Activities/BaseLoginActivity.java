fdf7a6a45f40d274e0ab13af432ee9ec6f600407
rahulyadav20111995 <rahulyadav574@gmail.com>
2017-01-04 23:56:55 +0530

    Google Sign IN DONE

--- app/src/main/java/in/evolve/ornateresidency/Activities/BaseLoginActivity.java
+++ app/src/main/java/in/evolve/ornateresidency/Activities/BaseLoginActivity.java
@@ -1,27 +1,39 @@
 package in.evolve.ornateresidency.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.util.Log;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import in.evolve.ornateresidency.R;
+public abstract class BaseLoginActivity extends AppCompatActivity  implements GoogleApiClient.OnConnectionFailedListener{
 
-public class BaseLoginActivity extends AppCompatActivity {
-
-    private GoogleApiClient googleApiClient;
-    private GoogleSignInOptions googleSignInOptions;
+     GoogleApiClient googleApiClient;
+     GoogleSignInOptions googleSignInOptions;
+    private static final String TAG = "BaseLoginActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_login);
+
 
         googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .requestProfile()
                 .build();
+
+        googleApiClient = new GoogleApiClient.Builder(BaseLoginActivity.this)
+                .enableAutoManage(BaseLoginActivity.this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG,"Unable to Connect To Google API Client");
     }
 }
--- app/src/main/java/in/evolve/ornateresidency/Activities/LoginActivity.java
+++ app/src/main/java/in/evolve/ornateresidency/Activities/LoginActivity.java
@@ -1,19 +1,53 @@
 package in.evolve.ornateresidency.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import in.evolve.ornateresidency.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseLoginActivity {
 
 
+    private SignInButton googleSignInButton;
+    private static final int RC_GOOGLE_SIGN_IN = 574;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        googleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_btn);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(signInIntent,RC_GOOGLE_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_GOOGLE_SIGN_IN && resultCode == RESULT_OK){
+
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.d("HRE IS","MAY BE NOT WORKING ");
+            if(googleSignInResult.isSuccess()){
+
+                GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+                Log.d("HRE IS",googleSignInAccount.getDisplayName());
+            }
+        }
     }
 }
