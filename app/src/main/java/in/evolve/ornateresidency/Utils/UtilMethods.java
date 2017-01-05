0086a7dcb9592af7fd93902e1e6fe3e191336e6e
rahulyadav20111995 <rahulyadav574@gmail.com>
2017-01-05 09:38:47 +0530

    Models Changed

--- app/src/main/java/in/evolve/ornateresidency/Utils/UtilMethods.java
+++ app/src/main/java/in/evolve/ornateresidency/Utils/UtilMethods.java
@@ -1,9 +1,29 @@
 package in.evolve.ornateresidency.Utils;
 
+import android.content.Context;
+import android.support.v4.view.GravityCompat;
+import android.view.Gravity;
+import android.widget.Toast;
+
 /**
  * Created by RAJEEV YADAV on 1/4/2017.
  */
 public class UtilMethods {
 
+    public static void toastS(Context context,String s){
 
+        Toast toast = new Toast(context);
+        toast.setText(s);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    public static void toastL(Context context,String s){
+        Toast toast = new Toast(context);
+        toast.setText(s);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
 }
